e1172d872cb2525b88653648fe33f2b863b81768
Gyks <gyksqop@gmail.com>
2020-03-12 18:33:15 +0300

    added validation for titles

--- models/validationSchemes.js
+++ models/validationSchemes.js
@@ -1,55 +1,97 @@
 // https://stackoverflow.com/questions/10656574/how-do-i-manage-mongodb-connections-in-a-node-js-web-application
-db.createCollection("users", {
+db.createCollection('users', {
   validator: {
     $jsonSchema: {
-      bsonType: "object",
-      required: ["nickname", "fullName", "seasonStatus", "links"],
+      bsonType: 'object',
+      required: ['nickname', 'fullName', 'seasonStatus', 'links'],
       properties: {
         nickname: {
-          bsonType: "string",
-          description: "must be a string and is required"
+          bsonType: 'string',
+          description: 'must be a string and is required'
         },
         fullName: {
-          bsonType: "object",
-          required: ["firstName"],
+          bsonType: 'object',
+          required: ['firstName'],
           properties: {
             firstName: {
-              bsonType: "string",
-              description: "must be a string and is required"
+              bsonType: 'string',
+              description: 'must be a string and is required'
             },
             secondName: {
-              bsonType: "string",
-              description: "must be a string if exists"
+              bsonType: 'string',
+              description: 'must be a string if exists'
             }
           }
         },
         seasonStatus: {
-          bsonType: "object",
-          required: ["current", "lastUpdated"],
+          bsonType: 'object',
+          required: ['current', 'lastUpdated'],
           properties: {
             current: {
-              enum: ["Active", "Inactive", "Trainee", "Support"],
-              description: "can only be one of the enum values and is required"
+              enum: ['Active', 'Inactive', 'Trainee', 'Support'],
+              description: 'can only be one of the enum values and is required'
             },
             lastUpdated: {
-              bsonType: "date",
-              description: "required and must be date"
+              bsonType: 'date',
+              description: 'required and must be date'
             }
           }
         },
         links: {
-          bsonType: "object",
-          required: ["vk", "discord"],
+          bsonType: 'object',
+          required: ['vk', 'discord'],
           properties: {
             discord: {
-              bsonType: "string",
-              description: "must be a string and is required"
+              bsonType: 'string',
+              description: 'must be a string and is required'
             },
             vk: {
-              bsonType: "string",
-              description: "must be a string if exists"
+              bsonType: 'string',
+              description: 'must be a string if exists'
             }
           }
+        }
+      }
+    }
+  }
+});
+
+db.createCollection('titles', {
+  bsonType: 'object',
+  required: [
+    'name',
+    'altNames',
+    'episodies',
+    'translators',
+    'editors',
+    'links'
+  ],
+  properties: {
+    name: {
+      bsonType: 'string',
+      description: 'must be a string and is required'
+    },
+    altNames: {
+      bsonType: 'array'
+    },
+    episodies: {
+      bsonType: 'int'
+    },
+    translators: {
+      bsonType: 'array'
+    },
+    editors: {
+      bsonType: 'array'
+    },
+    links: {
+      bsonType: 'object',
+      required: ['mal', 'shiki'],
+      properties: {
+        mal: {
+          bsonType: 'string'
+        },
+        shiki: {
+          bsonType: 'string'
         }
       }
     }
